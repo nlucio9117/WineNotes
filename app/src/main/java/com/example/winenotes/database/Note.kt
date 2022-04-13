be88d7b50f6090b739177f19662647a7c59b8692
nlucio9117 <nlucio9117@gmail.com>
2022-04-13 16:54:38 -0500

    create Note class

---
+++ app/src/main/java/com/example/winenotes/database/Note.kt
@@ -0,0 +1,18 @@
+package com.example.winenotes.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity
+class Note(
+    @PrimaryKey(autoGenerate = true) val id : Long,
+    @ColumnInfo val title : String,
+    @ColumnInfo val notes : String,
+    @ColumnInfo(name = "last_modified") val lastModified : String,
+) {
+    override fun toString(): String {
+        return "${title} ${notes} ${lastModified} (${id})"
+    }
+}
