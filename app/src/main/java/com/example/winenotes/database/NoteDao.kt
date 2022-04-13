aa4e58996cb4de0e417ea7b5a76752896bbd34d5
nlucio9117 <nlucio9117@gmail.com>
2022-04-13 17:02:36 -0500

    create NoteDao class

---
+++ app/src/main/java/com/example/winenotes/database/NoteDao.kt
@@ -0,0 +1,12 @@
+package com.example.winenotes.database
+
+import androidx.room.Dao
+import androidx.room.Query
+
+
+@Dao
+interface NoteDao {
+
+    @Query("SELECT * FROM note")
+    fun getAllNotes(): List<Note>
+}
