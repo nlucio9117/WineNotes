83c17851fc26f377d0b1e2c06debdc36ea0f0d0f
nlucio9117 <nlucio9117@gmail.com>
2022-04-14 14:46:30 -0500

    Add recylerview to layout

--- app/src/main/java/com/example/winenotes/MainActivity.kt
+++ app/src/main/java/com/example/winenotes/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.winenotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.winenotes.database.Note
 import com.example.winenotes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private val notes = mutableListOf<Note>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
