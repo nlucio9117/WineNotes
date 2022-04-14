efa9e611acc5c6c9296999daff19eec2418336f2
nlucio9117 <nlucio9117@gmail.com>
2022-04-14 15:58:43 -0500

    Set up all RecyclerView code

--- app/src/main/java/com/example/winenotes/MainActivity.kt
+++ app/src/main/java/com/example/winenotes/MainActivity.kt
@@ -2,17 +2,65 @@ package com.example.winenotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.winenotes.database.Note
 import com.example.winenotes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var adapter: MyAdapter
     private val notes = mutableListOf<Note>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-    }
-}
+
+        val layoutManager = LinearLayoutManager(this)
+        binding.recyclerview.setLayoutManager(layoutManager)
+
+        val dividerItemDecoration = DividerItemDecoration(
+            applicationContext, layoutManager.getOrientation()
+        )
+        binding.recyclerview.addItemDecoration(dividerItemDecoration)
+
+        adapter = MyAdapter()
+        binding.recyclerview.setAdapter(adapter)
+
+    }//this ends the onCreate function
+
+    inner class MyViewHolder(val view: TextView) :
+        RecyclerView.ViewHolder(view) {
+
+        }//this ends the MyViewHolder inner class
+
+    inner class MyAdapter :
+        RecyclerView.Adapter<MyViewHolder>() {
+
+        //loads item_view layout and ties it to a viewholder
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_view, parent, false) as TextView
+            return MyViewHolder(view)
+        }//this ends onCreateViewHolder
+
+        //updates data inside viewholder
+        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+            holder.view.text = notes[position].toString()
+        }//this ends onBindViewHolder
+
+        //returns the size of data
+        override fun getItemCount(): Int {
+            return notes.size
+        }//this ends getItemCount
+
+    }//this ends the MyAdapter inner class
+
+
+}//this ends the entire MainActivity
