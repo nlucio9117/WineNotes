58c82373014db4dddaec5eb241085bb7781425b7
nlucio9117 <nlucio9117@gmail.com>
2022-04-18 15:46:45 -0500

    Add name, date, course, task comments to MainActivity

--- app/src/main/java/com/example/winenotes/MainActivity.kt
+++ app/src/main/java/com/example/winenotes/MainActivity.kt
@@ -1,5 +1,10 @@
 package com.example.winenotes
 
+//Author: Natasha Lucio
+//Course: CSCI 4020-W1
+//Date: 18 April 2022
+//Task: Assignment 4
+
 import android.app.Activity
 import android.app.AlertDialog
 import android.content.Intent
